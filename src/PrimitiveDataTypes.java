498dbf797d0d075b4ad107e428fb829513b0c7c8
gangeshsingh72 <singh.gangesh@gmail.com>
2020-06-28 21:18:32 +0100

    PrimitiveData

---
+++ src/PrimitiveDataTypes.java
@@ -0,0 +1,23 @@
+public class PrimitiveDataTypes {
+  static byte b;
+  static short s;
+  static long l = 12345678901L;
+  static int i;
+
+    static float f = 123.01f;
+    static double d = 12345.99;
+    static char c;
+    static boolean bo;
+
+    public static void main(String[] args) {
+     System.out.println("Default value of byte is "+ b);
+        System.out.println("Default value of short is "+ s);
+        System.out.println("Default value of long is "+ l);
+        System.out.println("Default value of int is "+ i);
+        System.out.println("Default value of float is "+ f);
+        System.out.println("Default value of double is "+ d);
+        System.out.println("Default value of char is "+ c);
+        System.out.println("Default value of boolean is "+ bo);
+
+    }
+}
